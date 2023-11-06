5cd62a4c7a9322f5ebdf88fb402270a93484c736
NikosAndroulidakis <nikos.androulidakis@8bellsresearch.com>
2023-11-06 23:29:00 +0200

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,51 +1,23 @@
-let latitude = 0;
-let longitude = 0;
+let latitude = 37.983810;
+let longitude = 23.727539;
 
-navigator.geolocation.getCurrentPosition(function(position) {
-	latitude = position.coords.latitude;
-	longitude = position.coords.longitude;
-	
-	let map = L.map('map').setView([latitude, longitude], 14);		
+let updateCounter = 0;
 
-	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
-
-	let marker = L.marker([latitude, longitude]).addTo(map);
-	
-	marker.bindPopup("Responder <b>Nikolaos</b>").openPopup();
-	
-	var pointList = [];
-	
-	let firstpolyline = new L.Polyline(pointList, {
-    color: 'red',
-    weight: 3,
-    opacity: 0.5,
-    smoothFactor: 1
-});
-	
-
-	let updateCounter = 0;
-
-	function updateMarker(){
-		updateCounter += 1;
-		latitude = latitude + getRandomArbitrary(-0.5,+1)/10000;
-		longitude = longitude + getRandomArbitrary(-0.5,+1)/10000;
-		var newLatLng = new L.LatLng(latitude, longitude);
-		map.setView(newLatLng,14);
-		marker.setLatLng(newLatLng);
-		firstpolyline.addLatLng(newLatLng);
-		if(updateCounter === 1){
-			firstpolyline.addTo(map);
-		}
+function updateMarker(){
+	updateCounter += 1;
+	latitude = latitude + getRandomArbitrary(-0.5,+1)/10000;
+	longitude = longitude + getRandomArbitrary(-0.5,+1)/10000;
+	var newLatLng = new L.LatLng(latitude, longitude);
+	map.setView(newLatLng,14);
+	marker.setLatLng(newLatLng);
+	firstpolyline.addLatLng(newLatLng);
+	if(updateCounter === 1){
+		firstpolyline.addTo(map);
 	}
+}
 
-	window.setInterval(updateMarker, 100);
-	
-});
-		
-		
+window.setInterval(updateMarker, 100);
+			
 /**
  * Returns a random number between min (inclusive) and max (exclusive)
  */
